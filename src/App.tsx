651baab5aa3c56d27afac5b9d3a179cf2451c5a6
zahra <zsabah559@gmail.com>
2021-11-30 13:52:44 +0330

    Add routing system

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import VendorsList from "./components/VendorsList/VendorsList";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/Home/Home";
 
 function App() {
-  return <div><VendorsList/></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path={`/vendors`} element={<VendorsList />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return <Link to="/vendors">Show venders Lists...</Link>;
+};
+
+export default Home;
