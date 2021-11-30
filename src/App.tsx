e2f49c30c9e1bc2a93b5f244a8390442a1ca0f05
zahra <zsabah559@gmail.com>
2021-11-30 17:29:03 +0330

    Show vendors component in /venders path

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import React from "react";
-import VendorsList from "./components/VendorsList/VendorsList";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Vendors from "./pages/Vendors/Vendors";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path={`/vendors`} element={<VendorsList />}></Route>
+        <Route path={`/vendors`} element={<Vendors />}></Route>
       </Routes>
     </Router>
   );
--- src/pages/Vendors/Vendors.tsx
+++ src/pages/Vendors/Vendors.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import VendorsList from "../../components/VendorsList/VendorsList";
+import "./styles.scss";
 
 const Vendors = () => {
-  return <VendorsList />;
+  return (
+    <div className="vendors-page">
+      <VendorsList />
+    </div>
+    
+  );
 };
 
 export default Vendors;
