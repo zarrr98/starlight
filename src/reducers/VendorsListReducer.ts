9fa8029f334aa3d6c4c8890816393573658760ef
zahra <zsabah559@gmail.com>
2021-11-30 16:42:38 +0330

    Add redux reducer for the vendor's lists

--- src/reducers/VendorsListReducer.ts
+++ src/reducers/VendorsListReducer.ts
@@ -1,7 +1,34 @@
-import { VendorsListInterface } from "../providers/VendorsListProvider/VendorsListProvider";
+// import { VendorsListInterface } from "../providers/VendorsListProvider/VendorsListProvider";
+
+// export const vendorsListReducer = (
+//   state: VendorsListInterface,
+//   action: { type: string; payload: any }
+// ) => {
+//   switch (action.type) {
+//     case "value":
+//       return state;
+
+//     default:
+//       return state;
+//   }
+// };
+
+export interface VendorsListInterface {
+  configs: {
+    lat: number;
+    long: number;
+  };
+}
+
+const initialState: VendorsListInterface = {
+  configs: {
+    lat: 35.774,
+    long: 51.418,
+  },
+};
 
 export const vendorsListReducer = (
-  state: VendorsListInterface,
+  state = initialState,
   action: { type: string; payload: any }
 ) => {
   switch (action.type) {
