46e17a4d1314d111e85221d8c3418af9dcc737bf
zahra <zsabah559@gmail.com>
2021-11-30 20:27:35 +0330

    Get vendors from api and show a list of them

--- src/components/VendorsList/VendorsList.tsx
+++ src/components/VendorsList/VendorsList.tsx
@@ -1,8 +1,41 @@
-import React from "react";
-import './styles.scss';
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { VendorsListInterface } from "../../reducers/VendorsListReducer";
+import { vendorsList } from "../../utils/API";
+import VendorItem from "../VendorItem/VendorItem";
+import "./styles.scss";
 
 const VendorsList = () => {
-    return <div className="reddish">Vendors list :)))))))</div>
-}
+  const vendorsInfo = useSelector(
+    (state: { vendorsList: VendorsListInterface }) => state.vendorsList
+  );
+  const [vendors, setVendors] = useState([]);
+
+  useEffect(() => {
+    getVendersListFromServer();
+  }, [vendorsInfo.configs]);
+
+  const getVendersListFromServer = () => {
+    const resp = vendorsList(
+      1,
+      vendorsInfo.configs.lat,
+      vendorsInfo.configs.long
+    );
+    resp
+      .then((data) => {
+        console.log("data ------>", data);
+        setVendors(data.data.finalResult);
+      })
+      .catch((err) => console.log("Errrr :(((", err));
+  };
+
+  return (
+    <div className="venders-container">
+      {vendors.map((item: { data: any }, i) => {
+        return <VendorItem data={item.data} key={i} />;
+      })}
+    </div>
+  );
+};
 
 export default VendorsList;
