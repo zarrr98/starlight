8de424def48b8d54081f88c7ec86c7af4f657f22
zahra <zsabah559@gmail.com>
2021-12-01 11:57:05 +0330

    Add a function to check if the scroll bar is at the bottom of an element

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -2,3 +2,10 @@ export const addCommaInEveryThreeChars = (input: string | number) => {
   const stringInput = String(input);
   return stringInput.replace(/(\d)(?=(\d{3})+$)/g, "$1,");
 };
+
+export const hasScrollReachedBottom = (
+  scrollTop: number,
+  el: HTMLDivElement | null
+) => {
+  if (el) return scrollTop >= el?.scrollHeight - el?.clientHeight - 2;
+};
