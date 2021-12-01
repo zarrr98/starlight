88cc10d6f6ab46367903fcb2af4a15243610af5b
zahra <zsabah559@gmail.com>
2021-12-01 11:59:14 +0330

    Add pagination system in vendors lists

--- src/components/VendorsList/VendorsList.tsx
+++ src/components/VendorsList/VendorsList.tsx
@@ -1,39 +1,106 @@
-import React, { useEffect, useState } from "react";
+import React, { UIEventHandler, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { VendorsListInterface } from "../../reducers/VendorsListReducer";
 import { vendorsList } from "../../utils/API";
+import { hasScrollReachedBottom } from "../../utils/functions";
+import Loading from "../Loading/Loading";
 import VendorItem from "../VendorItem/VendorItem";
 import "./styles.scss";
 
-const VendorsList = () => {
+interface Props {
+  containerRef: { current: HTMLDivElement | null };
+}
+const VendorsList = (props: Props) => {
   const vendorsInfo = useSelector(
     (state: { vendorsList: VendorsListInterface }) => state.vendorsList
   );
-  const [vendors, setVendors] = useState([]);
+  const [pageNumber, setPageNumber] = useState(0);
+  const [hasNext, setHasNext] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [vendors, setVendors] = useState([] as any);
+
+  useEffect(() => {
+    getVendersListFromServer(true);
+  }, [vendorsInfo.configs]);
 
   useEffect(() => {
     getVendersListFromServer();
-  }, [vendorsInfo.configs]);
+  }, [pageNumber]);
 
-  const getVendersListFromServer = () => {
+  const getVendersListFromServer = (configsChanged = false) => {
+    setIsLoading(true);
+    let pageNum = getAndSetPageNumberBasedOnChangesInConfigs(configsChanged);
+    let prevData =
+      getAndSetVendorsPrevDataBasedOnChangesInConfigs(configsChanged);
     const resp = vendorsList(
-      1,
+      pageNum,
       vendorsInfo.configs.lat,
       vendorsInfo.configs.long
     );
     resp
       .then((data) => {
         console.log("data ------>", data);
-        setVendors(data.data.finalResult);
+        let newData = [...prevData, ...data.data.finalResult];
+        setVendors(newData);
+        setIsLoading(false);
+        setHasNext(!!data.data.finalResult.length);
       })
-      .catch((err) => console.log("Errrr :(((", err));
+      .catch((err) => {
+        setIsLoading(false);
+        setHasNext(false);
+        console.log("Errrr :(((", err);
+      });
   };
 
+  const getAndSetPageNumberBasedOnChangesInConfigs = (
+    configsChanged: boolean
+  ) => {
+    let pageNum = pageNumber;
+    if (configsChanged) {
+      pageNum = 0;
+      setPageNumber(0);
+      setHasNext(true);
+    }
+    return pageNum;
+  };
+
+  const getAndSetVendorsPrevDataBasedOnChangesInConfigs = (
+    configsChanged: boolean
+  ) => {
+    let data = vendors;
+    if (configsChanged) {
+      data = [];
+      setVendors([]);
+      setHasNext(true);
+    }
+    return data;
+  };
+
+  const scrollHandler = (e: any) => {
+    let scrollTop = e.target.scrollTop;
+    if (
+      hasScrollReachedBottom(scrollTop, props.containerRef.current) &&
+      !isLoading
+    ) {
+      setPageNumber((prev) => prev + 1);
+    }
+  };
+
+  useEffect(() => {
+    props.containerRef.current?.addEventListener("scroll", scrollHandler);
+    return () => {
+      props.containerRef.current?.removeEventListener("scroll", scrollHandler);
+    };
+  }, []);
+
   return (
-    <div className="venders-container">
-      {vendors.map((item: { data: any }, i) => {
-        return <VendorItem data={item.data} key={i} />;
+    <div className="venders-container" onScroll={scrollHandler}>
+      {vendors.map((item: { data: any; type: string }, i: number) => {
+        return (
+          item.type === "VENDOR" && <VendorItem data={item.data} key={i} />
+        );
       })}
+      {isLoading && <Loading active={isLoading} />}
     </div>
   );
 };
--- src/pages/Vendors/Vendors.tsx
+++ src/pages/Vendors/Vendors.tsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { useRef } from "react";
 import VendorsList from "../../components/VendorsList/VendorsList";
 import "./styles.scss";
 
 const Vendors = () => {
+  let vendorsPageElement = useRef(null);
   return (
-    <div className="vendors-page">
-      <VendorsList />
+    <div className="vendors-page" ref={vendorsPageElement}>
+      <VendorsList containerRef={vendorsPageElement} />
     </div>
-    
   );
 };
 
