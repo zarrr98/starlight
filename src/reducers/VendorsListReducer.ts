a003d896a3edbf8ebf6f85433548816181738a18
zahra <zsabah559@gmail.com>
2021-12-01 13:41:58 +0330

    Add vendors to store states and update them in reducer

--- src/reducers/VendorsListReducer.ts
+++ src/reducers/VendorsListReducer.ts
@@ -13,11 +13,25 @@
 //   }
 // };
 
+export const SET_VENDORS = "SET_VENDORS";
+export interface Vendor {
+  title: string;
+  logo: string;
+  backgroundImage: string;
+  coverPath: string;
+  voteCount: number;
+  rate: number;
+  discountValueForView: number;
+  description: string;
+  deliveryFee: number;
+}
+
 export interface VendorsListInterface {
   configs: {
     lat: number;
     long: number;
   };
+  vendors: Vendor[];
 }
 
 const initialState: VendorsListInterface = {
@@ -25,6 +39,7 @@ const initialState: VendorsListInterface = {
     lat: 35.774,
     long: 51.418,
   },
+  vendors: [],
 };
 
 export const vendorsListReducer = (
@@ -32,8 +47,8 @@ export const vendorsListReducer = (
   action: { type: string; payload: any }
 ) => {
   switch (action.type) {
-    case "value":
-      return state;
+    case SET_VENDORS:
+      return { ...state, vendors: action.payload.vendors };
 
     default:
       return state;
